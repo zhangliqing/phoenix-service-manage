6125202834a9ea069d5f5ef5553b77cca9d3d5bd
zhangliqing <623009470@qq.com>
2017-10-24 15:33:53 +0800

    remove lb rules.

--- index.js
+++ index.js
@@ -9,22 +9,34 @@ var removeInstance = function () {
     request.get({url:'http://117.50.1.134:8080/v2-beta/projects/1a3504/stacks/1st15/services'},function (err,httpResponse,body) {
       var date = new Date();
       console.log(date);
-      body_j = JSON.parse(body)
+      var body_j = JSON.parse(body)
       var services = body_j.data;
-      for(var i = 0; i < services.length; i++){
-        if((date-Date.parse(services[i].created)) > 300000){
-          //console.log('delete '+services[i].id);
-          request.del({url: 'http://117.50.1.134:8080/v2-beta/projects/1a3504/services/'+services[i].id}, function (err, httpResponse, body) {
-            if(err){
-              console.log('Internal error.');
+      request.get({url:'http://117.50.1.134:8080/v2-beta/projects/1a3504/loadbalancerservices/1s18'},function (err,httpResponse,body1) {
+        var proxyData = JSON.parse(body1)
+        for(var i = 0; i < services.length; i++){
+          if((date-Date.parse(services[i].created)) > 300000){
+            request.del({url: 'http://117.50.1.134:8080/v2-beta/projects/1a3504/services/'+services[i].id}, function (err, httpResponse, body) {
+              if(err){
+                console.log('Internal error.');
+              }
+            })
+            for(var j = 0; j<proxyData.lbConfig.portRules.length; j++){
+              if(proxyData.lbConfig.portRules[j].hostname!=null && proxyData.lbConfig.portRules[j].hostname.indexOf(services[i].name)!=-1){
+                proxyData.lbConfig.portRules.splice(j,1)
+                break
+              }
             }
-          })
+          }
         }
-      }
+        request.put({
+          url: 'http://117.50.1.134:8080/v2-beta/projects/1a3504/loadbalancerservices/1s18',
+          json: proxyData
+        })
+      })
     })
     request.get({url:'http://117.50.1.134:8080/v2-beta/projects/1a3504/containers/'},function (err,httpResponse,body) {
       var date = new Date();
-      body_j = JSON.parse(body)
+      var body_j = JSON.parse(body)
       var containers = body_j.data;
 
       for(var i = 0; i < containers.length; i++){
