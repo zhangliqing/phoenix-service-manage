21f8fb919481856873316b7387684ec88bd10565
zhangliqing <623009470@qq.com>
2017-11-09 13:42:42 +0800

    env

--- index.js
+++ index.js
@@ -3,25 +3,31 @@
  */
 var request = require('request')
 
+const ENDPOINT = 'http://10.2.253.121:8080/v2-beta'
+const ENV = '1a51'
+const LBID = '1s16'
+const WSPREFIX = 'ws://10.2.253.122:83'
+const STACKID = '1st16'
+
 var removeInstance = function () {
   setTimeout(function () {
     console.log('loop');
-    request.get({url:'http://117.50.1.134:8080/v2-beta/projects/1a3504/stacks/1st15/services'},function (err,httpResponse,body) {
+    request.get({url:ENDPOINT + '/projects/' + ENV +'/stacks/'+ STACKID +'/services'},function (err,httpResponse,body) {
       var date = new Date();
       console.log(date);
       var body_j = JSON.parse(body)
       var services = body_j.data;
-      request.get({url:'http://117.50.1.134:8080/v2-beta/projects/1a3504/loadbalancerservices/1s18'},function (err,httpResponse,body1) {
+      request.get({url: ENDPOINT + '/projects/' + ENV + '/loadbalancerservices/' + LBID},function (err,httpResponse,body1) {
         var proxyData = JSON.parse(body1)
         for(var i = 0; i < services.length; i++){
           if((date-Date.parse(services[i].created)) > 300000){
-            request.del({url: 'http://117.50.1.134:8080/v2-beta/projects/1a3504/services/'+services[i].id}, function (err, httpResponse, body) {
+            request.del({url: ENDPOINT + '/projects/' + ENV + '/services/'+services[i].id}, function (err, httpResponse, body) {
               if(err){
                 console.log('Internal error.');
               }
             })
             for(var j = 0; j<proxyData.lbConfig.portRules.length; j++){
-              if(proxyData.lbConfig.portRules[j].hostname!=null && proxyData.lbConfig.portRules[j].hostname.indexOf(services[i].name)!=-1){
+              if(proxyData.lbConfig.portRules[j].path!=null && proxyData.lbConfig.portRules[j].path.indexOf(services[i].name)!=-1){
                 proxyData.lbConfig.portRules.splice(j,1)
                 break
               }
@@ -29,20 +35,19 @@ var removeInstance = function () {
           }
         }
         request.put({
-          url: 'http://117.50.1.134:8080/v2-beta/projects/1a3504/loadbalancerservices/1s18',
+          url: ENDPOINT + '/projects/' + ENV + '/loadbalancerservices/' + LBID,
           json: proxyData
         })
       })
     })
-    request.get({url:'http://117.50.1.134:8080/v2-beta/projects/1a3504/containers/'},function (err,httpResponse,body) {
+    request.get({url:ENDPOINT + '/projects/' + ENV + '/containers/'},function (err,httpResponse,body) {
       var date = new Date();
       var body_j = JSON.parse(body)
       var containers = body_j.data;
 
       for(var i = 0; i < containers.length; i++){
         if((date-Date.parse(containers[i].created)) > 300000 && containers[i].labels.container_type == 'cloudware'){
-          //console.log('delete '+containers[i].id);
-          request.del({url: 'http://117.50.1.134:8080/v2-beta/projects/1a3504/containers/'+containers[i].id}, function (err, httpResponse, body) {
+          request.del({url: ENDPOINT + '/projects/' + ENV + '/containers/'+containers[i].id}, function (err, httpResponse, body) {
             if(err){
               console.log('Internal error.');
             }
